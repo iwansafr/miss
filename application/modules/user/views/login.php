daf0d49f758891a1a8b56557ba02ddbc4fc940e3
iwansafr <iwansafr@gmail.com>
2020-02-06 07:42:04 +0700

    remove register and forget password

--- application/modules/user/views/login.php
+++ application/modules/user/views/login.php
@@ -57,8 +57,6 @@
           <button class="btn btn-primary btn-block">Login</button>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
         </div>
       </div>
     </div>
